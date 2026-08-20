be5b0cb7beb70f071e68d2ecce938b15ed44a88a
Alvaro <alvaroandradesantos2009@gmail.com>
2026-08-20 10:39:59 -0300

    5 - adicionando detalhes

--- ex2.py
+++ ex2.py
@@ -6,7 +6,7 @@ class Produto:
         self.nome = nome
         self.preco = preco
     def mostrar(self):
-        print(f"Produto: {self.nome} |  Preço: {self.preco}")
+        print(f"Produto: {self.nome}\nPreço: {self.preco}\n")
         
 def cadProd():
     nomeProd = input("Insira o nome do produto: ")
@@ -21,6 +21,7 @@ def cadProd():
             print("Valor inválido! Insira apenas valores numéricos")
     produtoCadastro = Produto(nomeProd, precoProd)
     listProd.append(produtoCadastro)
+    print(f"Produto cadastrado!")
     
 def listarProds():
     for produto in listProd:
@@ -30,12 +31,15 @@ def listarProds():
 def comprarProd():
     while True:
         try:
+            #pega o produto já cadastrado
             p = listProd[int(input("Insira o índice do produto: "))]
             break
         except IndexError:
             print("Índice inválido! Insira um índice que exista! (lembre-se: o índice sempre começa pelo 0!)")
         except ValueError:
             print("Valor inválido! Insira um valor numérico válido!")
+    #exibe o produto selecionado
+    print(f"Produto selecionado: {p.nome} Preço: {p.preco}")
     while True:
         try:
             qtd = int(input("Insira a quantidade que deseja pagar: "))
@@ -46,15 +50,12 @@ def comprarProd():
     valorTotal = pPreco * qtd
     print(f"O valor total a pagar será: {valorTotal}")
     if valorTotal >= 100:
-        print("Desconto disponível!")
+        print("Desconto disponível!\n")
     else:
-        print("Desconto indisponível!")
-    
-def encerrarPrograma():
-    print("Encerrando programa...")
-
+        print("Desconto indisponível!\n")
+#loop principal
 while True:
-    escolha = input("o que deseja fazer? 1 para cadastrar produtos|2 para comprar produtos|3 para listar produtos|4 para encerrar programa: ")
+    escolha = input("O que deseja fazer?\n[1] para cadastrar produtos|[2] para comprar produtos|[3] para listar produtos|[4] para encerrar programa: ")
     if escolha == "1":
         cadProd()
     elif escolha == "2":
@@ -62,7 +63,7 @@ while True:
     elif escolha == "3":
         listarProds()
     elif escolha == "4":
-        encerrarPrograma()
+        print("\nEncerrando programa...\n")
         break
     else:
-        print("Digite uma das opções válidas")
+        print("Digite uma das opções válidas\n")
